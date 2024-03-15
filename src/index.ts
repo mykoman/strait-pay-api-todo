ca9c7c2ec88da7cdc0f49ccf659f30dc280a3fde
Michael Ogbuma <ogbumamichael@gmail.com>
2024-03-15 04:45:57 +0100

    - use cors body parser - use .env file - configure database

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
 import express, { Request, Response } from 'express';
+import mongoose from "mongoose";
+import cors from "cors";
+import { json } from "body-parser";
+import { config } from 'dotenv'
 
 const app = express();
-const PORT = 3003;
+config();
+const PORT = process.env.PORT || 3003;
+const dbUrl: string = process.env.DATABASE_URL!;
+
+app.use(cors());
+app.use(express.json());
+
+app.use(json());
+
+mongoose.connect(dbUrl, {
+  retryWrites: true,
+  w: "majority",
+});
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
